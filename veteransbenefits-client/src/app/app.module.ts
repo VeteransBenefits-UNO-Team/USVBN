86baff303bb3db70d8d584e4b543a89d467d2f1d
tbossert2 <tbossert2@gmail.com>
2023-09-28 14:42:59 -0500

    Milestone 1 - Bossert FAQ/Resources html and CSS

--- veteransbenefits-client/src/app/app.module.ts
+++ veteransbenefits-client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FaqComponent } from './faq/faq.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaqComponent,
+    ResourcesComponent
   ],
   imports: [
     BrowserModule
