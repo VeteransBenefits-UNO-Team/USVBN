8b1b58037890a497412510c858d4ec8bf34743ae
Trevin Kotinek <trevinkotinek@gmail.com>
2023-10-09 18:42:35 -0500

    Refactor front-end to use .scss style sheets

--- veteransbenefits-client/src/app/resources/resources.component.ts
+++ veteransbenefits-client/src/app/resources/resources.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-resources',
   templateUrl: './resources.component.html',
-  styleUrls: ['./resources.component.css']
+  styleUrls: ['./resources.component.scss']
 })
 export class ResourcesComponent {
 
