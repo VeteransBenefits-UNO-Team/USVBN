1ce8af3eef18f6e0f4fcba892340f3b494f375c2
mwebb3005 <mwebb3005@gmail.com>
2023-10-10 23:14:26 -0500

    Setup Placeholder Search Bar

--- veteransbenefits-client/src/app/app.module.ts
+++ veteransbenefits-client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FaqComponent } from './faq/faq.component';
@@ -31,6 +32,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
     BrowserModule,
     RouterModule.forRoot(routes),
     ReactiveFormsModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatStepperModule,
