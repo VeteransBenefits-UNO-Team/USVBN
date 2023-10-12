d46fe609edf89d9f8c1c01dae6c9fde2db957eb6
mwebb3005 <mwebb3005@gmail.com>
2023-10-11 23:37:08 -0500

    Added to Resource Page

--- veteransbenefits-client/src/app/resources/resources.component.ts
+++ veteransbenefits-client/src/app/resources/resources.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./resources.component.scss']
 })
 export class ResourcesComponent {
+  searchInput: string = ''; // Property to hold the search input
+  resourceItems = [  // Define your resource items with unique IDs
+    { id: 1, question: 'What is USVBN?', answer: '...' },
+    { id: 2, question: 'How do I start?', answer: '...' },
+    // Add more resource items with unique IDs
+  ];
 
+  searchQuestions() {
+    console.log(this.searchInput.toLowerCase());
+    const searchTerm = this.searchInput.toLowerCase();
+
+    // Filter the resource items based on the search input
+    const searchResults = this.resourceItems.filter(resource =>
+      resource.question.toLowerCase().includes(searchTerm) ||
+      resource.answer.toLowerCase().includes(searchTerm)
+    );
+  }
 }
