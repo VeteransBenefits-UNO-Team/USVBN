a2322924766e70d642f8dda1864a49b1370c9d0d
mwebb3005 <mwebb3005@gmail.com>
2023-11-05 21:38:32 -0600

    Homepage SlideShow V1 (WIP)

--- veteransbenefits-client/src/app/home/home.component.ts
+++ veteransbenefits-client/src/app/home/home.component.ts
@@ -7,12 +7,41 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor(private router:Router){}
+  constructor(private router: Router) {}
+
   ngOnInit(): void {}
 
-  getFaq(){
+  getFaq() {
     return this.router.navigateByUrl('/faq');
   }
 
+  ngAfterViewInit(): void {
+    this.slideshow();
+  }
 
+  slideshow() {
+    let index: number = 0;
+    displayImages();
+
+    function displayImages() {
+      let i: number;
+      const images: HTMLCollectionOf<Element> = document.getElementsByClassName('image');
+
+      for (i = 0; i < images.length; i++) {
+        const image = images[i] as HTMLElement;
+        image.style.display = 'none';
+      }
+
+      index++;
+
+      if (index > images.length) {
+        index = 1;
+      }
+
+      const currentImage = images[index - 1] as HTMLElement;
+      currentImage.style.display = 'block';
+
+      setTimeout(displayImages, 5000);
+    }
+  }
 }
