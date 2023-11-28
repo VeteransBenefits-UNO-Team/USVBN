7a5566bf7caed1fd60e71b926bcfd3ce297e9354
cechirino <cechirino96@gmail.com>
2023-11-28 13:13:36 -0600

    update

--- veteransbenefits/veteransbenefits/src/main/java/com/veteransbenefitsapi/veteransbenefits/controller/auth/CAuth.java
+++ veteransbenefits/veteransbenefits/src/main/java/com/veteransbenefitsapi/veteransbenefits/controller/auth/CAuth.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CAuth
 {
     @Autowired
